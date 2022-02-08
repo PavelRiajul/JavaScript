ce72375f6b7cb65bf09e87adb8941944e16ee2a0
Riajul Islam <riajulpislam@gmail.com>
2022-02-08 20:19:04 +0600

    number

--- Variable/var.js
+++ Variable/var.js
@@ -1,6 +1,2 @@
-var some_thing = 'riajul islam pavel';
-console.log(some_thing)
-var My_name = 'emadul islam fahim';
-console.log(My_name)
-var my_name = 'riajul islam pavel'
-console.log(My_name)
+//******Numbers*****
+var n = 1459
