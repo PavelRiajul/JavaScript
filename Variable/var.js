5b94aab6c0c7307409d8a2acc01affeb624bad39
Riajul Islam <riajulpislam@gmail.com>
2022-02-08 16:51:20 +0600

    variable

--- Variable/var.js
+++ Variable/var.js
@@ -1,15 +1,6 @@
-var name1 = '2';
-console.log(name1 ** 0.5);
-var my_name = ('riajul islam pavel');
-console.log(my_name)
-var $text = ('tamirul millat kamill madrasah');
-console.log($text)
-
-var x = 20;
-var y = 50;
-var z = x+y;
-console.log(z)
-
-
-var $name = 'riajul islam pavel';
-console.log($name)
+var some_thing = 'riajul islam pavel';
+console.log(some_thing)
+var My_name = 'emadul islam fahim';
+console.log(My_name)
+var my_name = 'riajul islam pavel'
+console.log(My_name)
