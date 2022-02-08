b8177c42502f00b28d7c49a2f335645e42734693
Riajul Islam <riajulpislam@gmail.com>
2022-02-08 16:45:04 +0600

    var

--- Variable/var.js
+++ Variable/var.js
@@ -1,6 +1,15 @@
-var name = ('riajul islam');
-var age = ('20');
-var madrasah = ('tamirul millat kamill madrasah');
-console.log('My name is' + name);
-console.log('i am'+ age);
-console.log('My school name is' + madrasah);
+var name1 = '2';
+console.log(name1 ** 0.5);
+var my_name = ('riajul islam pavel');
+console.log(my_name)
+var $text = ('tamirul millat kamill madrasah');
+console.log($text)
+
+var x = 20;
+var y = 50;
+var z = x+y;
+console.log(z)
+
+
+var $name = 'riajul islam pavel';
+console.log($name)
