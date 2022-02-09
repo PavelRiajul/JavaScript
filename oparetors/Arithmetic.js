85247a6ca96423739bd9634f0f9e9d75bd4fcf82
Riajul Islam <riajulpislam@gmail.com>
2022-02-09 20:23:16 +0600

    arithmetic

---
+++ oparetors/Arithmetic.js
@@ -0,0 +1,12 @@
+/*var a = 13
+var b = 5
+console.log(a+b)
+console.log(b-a)
+console.log(a*b)
+console.log(a/b)
+console.log(a++)
+console.log(a)
+console.log(a%b)*/
+var a = 23
+var b = 5
+console.log(a%b)
