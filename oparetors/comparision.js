d8a088aa2c0ede21ec48073c70c6495b11d8de3c
Riajul Islam <riajulpislam@gmail.com>
2022-02-09 20:56:43 +0600

    comparisom

--- oparetors/comparision.js
+++ oparetors/comparision.js
@@ -1,3 +1,11 @@
-var a = 20
-var b = 10
+/*var a = 20
+var b = 20
 console.log(a == b)
+console.log(a != b)*/
+var a = 30
+var b = 40
+console.log(a == b)
+console.log(a != b)
+
+console.log(a >= b)
+console.log(a <= b)
