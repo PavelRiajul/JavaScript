87333d4012e6889c9d7c84f8b3dc456dbb8b1729
Riajul Islam <riajulpislam@gmail.com>
2022-02-09 15:47:23 +0600

    increment

--- data-type/oparetors/Arithmetic.js
+++ data-type/oparetors/Arithmetic.js
@@ -3,7 +3,17 @@ var b = 50
 console.log(a+b)
 console.log(b-a)
 console.log(a*b)
-console.log(a/b)*/
+console.log(a/b)
+
+//........Mudolus...
 var  a = 23
 var b = 4
-console.log(a%b)
+console.log(a%b)*/
+
+//....Incremental oparetor...
+ var a = 15
+ console.log(a++)
+ console.log(a)
+
+ //......dicrement oparetor....
+ 
