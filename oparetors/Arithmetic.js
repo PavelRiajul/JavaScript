3cadf68c5e076d3561e78c7b89527a00dfbe6a5b
Riajul Islam <riajulpislam@gmail.com>
2022-02-10 19:15:47 +0600

    assignment

--- oparetors/Arithmetic.js
+++ oparetors/Arithmetic.js
@@ -1,16 +1,13 @@
-/*var a = 13
-var b = 5
+var a = 10
+var b = 31
 console.log(a+b)
-console.log(b-a)
+console.log(b - a)
 console.log(a*b)
-console.log(a/b)
+console.log(b/a)
+console.log(b%a)
+var a  = 15
+var b = 25
 console.log(a++)
 console.log(a)
-console.log(a%b)
-var a = 23
-var b = 5
-console.log(a%b)*/
-var a =20
-var b = 5
-console.log(a--)
-console.log(a)
+console.log(b--)
+console.log(b)
