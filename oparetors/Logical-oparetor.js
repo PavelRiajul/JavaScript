b82715c7df05197a9262f1ff9540ba00d6453318
Riajul Islam <riajulpislam@gmail.com>
2022-02-10 18:18:46 +0600

    daily commit

---
+++ oparetors/Logical-oparetor.js
@@ -0,0 +1,11 @@
+/*var a = 20
+var b = 10
+var c = 50
+var d = 30
+console.log((a > b) && (c > d))
+console.log((a > b) || (c<d) )
+console.log(!(a<b))*/
+var a = 10
+var b = 20
+var c = a > b
+console.log(c)
--- oparetors/comparision.js
+++ oparetors/comparision.js
@@ -1,8 +1,12 @@
-var a = 9
+/*var a = 9
 var b = 10
 console.log(a == b) //false
 console.log(a != b) // true
 console.log(a > b)// true
 console.log(a < b)//false
 console.log(a >= b) //true
-console.log(a <= b) //false
+console.log(a <= b) //false*/
+var a = '30'
+var b = 30
+console.log(a === b)
+console.log(a !== b)
