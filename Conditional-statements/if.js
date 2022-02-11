5bc088e7941df68cea46e802aca3cc846e66331e
Riajul Islam <riajulpislam@gmail.com>
2022-02-11 22:18:41 +0600

    if statement

--- Conditional-statements/if.js
+++ Conditional-statements/if.js
@@ -1,3 +1,10 @@
-if(3 < 5){
-    console.log('riajul islam pavel')
+/*if(13 > 12){
+    console.log('Riajul Islam Pavel')
+}*/
+if(19 > 10) {
+    console.log('greater')
+}
+var age = 20
+if(age >= 20){
+    console.log('tamirul millat kamill madrasah')
 }
