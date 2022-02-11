7eb6ca050732d461f4c4ed2ef46b6a999868e12b
Riajul Islam <riajulpislam@gmail.com>
2022-02-11 22:59:44 +0600

    if

--- Conditional-statements/if.js
+++ Conditional-statements/if.js
@@ -1,10 +1,16 @@
 /*if(13 > 12){
     console.log('Riajul Islam Pavel')
-}*/
+}
 if(19 > 10) {
     console.log('greater')
 }
 var age = 20
 if(age >= 20){
     console.log('tamirul millat kamill madrasah')
+
+}*/
+var a = 40
+var b = 20
+if(a > b){
+    console.log(a + 'is gretarthen' + b)
 }
