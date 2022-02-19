6d8fcb5a7cc0992f244dc605b74e5c7441696a33
Riajul Islam <riajulpislam@gmail.com>
2022-02-19 21:15:02 +0600

    js variable

---
+++ Variable/var.js
@@ -0,0 +1,7 @@
+var age = 20
+var _myName = 'Riaju islam pavel'
+var $collage = 'tamirul millat kamill madrasa'
+
+console.log(_myName +'i am 20 years old')
+console.log('My name is'+ _myName)
+console.log('My collage name is' + $collage)
