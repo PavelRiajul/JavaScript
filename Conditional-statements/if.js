cf717d71e5eb8e6a845b2006b361dffcfcec7e42
Riajul Islam <riajulpislam@gmail.com>
2022-02-19 23:29:30 +0600

    if statement

--- Conditional-statements/if.js
+++ Conditional-statements/if.js
@@ -8,7 +8,7 @@ var age = 20
 if(age >= 20){
     console.log('tamirul millat kamill madrasah')
 
-}*/
+}
 
     var n = 10
     var n = 20
@@ -17,7 +17,31 @@ if(age >= 20){
     }
     if(20 - 10 == 10){
         console.log('tamirul millat kamill madrasah')
+    }*/
+    if(18 > 10){
+        console.log('Riajul islam pavel')
     }
+    if(5<7){
+        console.log('tamirul millat kamill madrasah')
+    }
+    var x = 20
+    var x = 10
+    if(20 - 10 == 10){
+        console.log('My name is riajul islm pavel' +'i am 20 years old')
+    }
+    var x = 'Fahim'
+    if(x === 'Fahim'){
+        console.log('My contry is bangladesh')
+    }
+    var age = 20
+    if(age >= 20){
+        console.log('lorem')
+    }
+    var my_age = 30
+    if(my_age >20 && my_age<50){
+        console.log('tamirul millat kamill madrasah')
+    }
+
 
 
 
