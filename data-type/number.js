7b843729acaf2eb3ede6902c1ea9f9e8b127b721
Riajul Islam <riajulpislam@gmail.com>
2022-02-19 21:35:10 +0600

    js number

--- data-type/number.js
+++ data-type/number.js
@@ -6,8 +6,17 @@ console.log(Number.parseInt(nn))
 console.log(Number.MAX_VALUE)
 console.log(Number.MIN_SAFE_INTEGER)
 console.log(1/0)
-console.log('asdf' *4)*/
+console.log('asdf' *4)
 var nn = Number(55.99)
 console.log(nn)
 console.log(Number.parseInt(nn))
+console.log(Number.MAX_VALUE)*/
+var i = 22
+var l = 99.99
+var nn = Number(44.44)
+console.log(nn)
+console.log(Number.parseFloat(nn))
 console.log(Number.MAX_VALUE)
+console.log(Number.MIN_SAFE_INTEGER)
+console.log(1/0)
+console.log('pavel' *3)
