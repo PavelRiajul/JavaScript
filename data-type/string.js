94f33938fa4858b4993fceb2b84d2adf973f6ce1
Riajul Islam <riajulpislam@gmail.com>
2022-02-19 21:21:46 +0600

    number

--- data-type/string.js
+++ data-type/string.js
@@ -9,5 +9,9 @@ console.log(typeof (age + ''))
 var complex = 20 + 40 + 70 + 100 + 'riajul islam' + 10
 console.log(complex)*/
 
-var age = 20
-console.log(typeof (age + ''))
+/*var age = +'20'
+var name = 30
+console.log(typeof age)
+console.log(typeof(age + ''))*/
+var age = '20'
+console.log(typeof age)
