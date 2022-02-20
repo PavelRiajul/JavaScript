1364fca2689e6775120f030da48b282835aeb200
Riajul Islam <riajulpislam@gmail.com>
2022-02-21 01:46:18 +0600

    if-else-statement

--- Conditional-statements/if-else-statement.js
+++ Conditional-statements/if-else-statement.js
@@ -1,8 +1,28 @@
-var a = 21
+/*var a = 22
 var b = 11
 if(a / b == 2){
     console.log('good morning .good after non.')
 }
 else{
     console.log('good evening. good night. good day')
+}
+if(a % b == 2){
+    console.log(' hi riajul islam')
+}
+else{
+    console.log('Hi imadul islam')
+}*/
+var x = 55
+var z = 10
+if(x / z === 5){
+    console.log('tamirul millat kamill madrasah')
+}
+else{
+    console.log('medda islamia alim madrasah')
+}
+if(x % z === 5){
+    console.log('tanzimul islamia kedete madrasah')
+}
+else{
+    console.log('my name is pavel')
 }
