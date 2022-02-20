2e82a2d7bb8a3f7a8c26c358cff08076379d684a
Riajul Islam <riajulpislam@gmail.com>
2022-02-21 02:08:25 +0600

    if-else-if-statement

---
+++ Conditional-statements/if-else-if-statement.js
@@ -0,0 +1,13 @@
+var date = 'sunday'
+if(date === 'friday'){
+    console.log('riajul sislam pavel')
+}
+else if(date === 'thursday'){
+    console.log('sirajul islam parvez')
+}
+else if(date === 'monday'){
+    console.log('imadul islam fahim')
+}
+else{
+    console.log('monowara moni')
+}
