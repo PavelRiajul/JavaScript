65c6560e09ec62d2fd491bafcb6b3afb0891f274
Riajul Islam <riajulpislam@gmail.com>
2022-02-21 02:23:51 +0600

    else-if

--- Conditional-statements/if-else-if-statement.js
+++ Conditional-statements/if-else-if-statement.js
@@ -1,4 +1,4 @@
-var date = 'sunday'
+/*var date = 'sunday'
 if(date === 'friday'){
     console.log('riajul sislam pavel')
 }
@@ -10,4 +10,27 @@ else if(date === 'monday'){
 }
 else{
     console.log('monowara moni')
+}
+var num = 20
+if(num > 1){
+    if(num > 10){
+        console.log('good morning')
+    }
+    else{
+        console.log('good night')
+    }
+}*/
+var age = 40
+if(age > 30){
+    if(age > 50){
+        console.log('riajul islam pavel')
+    }
+    else if(age > 70){
+        console.log(
+            'sirajul islam parvez'
+        )
+    }
+    else{
+        console.log('imadul islam')
+    }
 }
