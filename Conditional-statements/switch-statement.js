320e926d4f6bf7545eea6faf2f5d0b6088721ae0
Riajul Islam <riajulpislam@gmail.com>
2022-02-21 03:15:57 +0600

    switch-statement

--- Conditional-statements/switch-statement.js
+++ Conditional-statements/switch-statement.js
@@ -1,4 +1,4 @@
-var date = new Date()
+/*var date = new Date()
 //0-sunday, 1-monday, 2-tuesday
 var today = date.getDay()
 
@@ -13,4 +13,34 @@ switch(today) {
             console.log('today is friday')
         break
                 
+}
+var weekday = 'friday'
+
+switch(weekday){
+    case 'suterday':
+        console.log('It is suterday')
+        break
+        case 'sunday':
+            console.log('It is sunday')
+            break
+            case 'friday':
+                console.log('It is friday')
+                break
+                default:
+                console.log('not a day')
+                break
+
+}*/
+var rollno = 3
+switch(rollno){
+    case 1:
+        console.log('riajul')
+        break
+        case 2:
+        console.log('islam')
+        break
+        case 3:
+        console.log('pavel')
+        
+            
 }
