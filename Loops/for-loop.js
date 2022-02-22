83dff56abea9f75707a786d5d59b872d8c50e8aa
Riajul Islam <riajulpislam@gmail.com>
2022-02-22 17:03:06 +0600

    odd-even numver

--- Loops/for-loop.js
+++ Loops/for-loop.js
@@ -1,3 +1,13 @@
-for(var a = 10; a <200 ; a+=100){
+/*for(var a = 10; a <200 ; a+=100){
+
     console.log( (a + 1) +'riajul islam pavel')
+} 
+//1 to 100 print
+for(var a = 0; a < 100; a++){
+    console.log(a)
+}*/
+for(var i = 1; i <100; i++){
+    if(i%2 == 0){
+        console.log(i)
+    }
 }
