e9b0f10609f611a49483b8a7777d3ebdd3d73ab6
Riajul Islam <riajulpislam@gmail.com>
2022-02-22 22:23:56 +0600

    break

--- Loops/break.js
+++ Loops/break.js
@@ -1,8 +1,14 @@
 
-for(var a = 1; a < 10; a++){
+/*for(var a = 1; a < 10; a++){
     if(a % 5 == 0){
         break
     }else{
         console.log(a)
     }
+}*/
+for(var a = 1; a < 10; a++) {
+if(a %5 == 0){
+    break
+}else{console.log(a)}
+
 }
