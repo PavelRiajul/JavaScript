075083b8ee93790eace66c728019f88eac3f584f
Riajul Islam <riajulpislam@gmail.com>
2022-02-22 17:13:07 +0600

    sum

--- Loops/for-loop.js
+++ Loops/for-loop.js
@@ -2,12 +2,21 @@
 
     console.log( (a + 1) +'riajul islam pavel')
 } 
+//odd number
 //1 to 100 print
 for(var a = 0; a < 100; a++){
     console.log(a)
-}*/
+}
+//even number
 for(var i = 1; i <100; i++){
     if(i%2 == 0){
         console.log(i)
     }
-}
+}*/
+//sumition(sum)
+var sum = 0;
+for(var i = 1; i < 10; i++){
+    console.log(sum + '+' + i + '=' + (sum+i))
+    sum += i
+} 
+console.log('result =' + sum)
