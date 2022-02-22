694d92325378a732b374e8a8fa89a852d6da98ae
Riajul Islam <riajulpislam@gmail.com>
2022-02-22 20:57:28 +0600

    all loop

--- Loops/for-loop.js
+++ Loops/for-loop.js
@@ -2,7 +2,7 @@
 
     console.log( (a + 1) +'riajul islam pavel')
 } 
-//odd number
+
 //1 to 100 print
 for(var a = 0; a < 100; a++){
     console.log(a)
@@ -12,7 +12,7 @@ for(var i = 1; i <100; i++){
     if(i%2 == 0){
         console.log(i)
     }
-}*/
+}
 //sumition(sum)
 var sum = 0;
 for(var i = 1; i < 10; i++){
@@ -20,3 +20,34 @@ for(var i = 1; i < 10; i++){
     sum += i
 } 
 console.log('result =' + sum)
+//odd Number
+for(var a = 9; a < 52; a++){
+    if(a % 4 == 3){
+        console.log(a)
+    }
+}
+//sum
+var sum = 0
+for(var a = 1; a < 10; a++){
+    if(a % 2 == 0){
+        console.log(sum + '+' + a + '=' + (sum+a))
+        sum += a
+    }
+}
+console.log('result =' +sum)
+for(var a = 10; a <200; a += 10){
+    console.log( (a +100) +'riajul islam' + 20)
+}
+for(var i = 1; i < 20; i++){
+    if(i % 2 == 1){
+        console.log(i)
+    }
+}*/
+var sum = 1
+for( var a = 1; a < 10; a++){
+    if(a % 2 == 0){
+        console.log(sum + '+' + a + '=' + (sum +a))
+        sum += a
+    }
+}
+console.log('result =' +sum)
