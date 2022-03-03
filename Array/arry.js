7736aa04fd356f23108beb9b6961aad407b360bb
Riajul Islam <riajulpislam@gmail.com>
2022-03-03 16:01:28 +0600

    arry loop use

--- Array/arry.js
+++ Array/arry.js
@@ -1,9 +1,5 @@
-var age = [1,2,3,4,5]
-age[5] = 10
-age[8] =11
-age[2] = 100
-console.log(age)
-console.log(age[7])
-console.log(age[0])
-console.log(age.length)
+var name = ['pavel', 'Riajul', 'fahim', 'yamoli']
 
+for(var a = 0; a< name.length; a++){
+    console.log('Nmae:' + name[a])
+}
