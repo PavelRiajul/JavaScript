43d67e6d9f81f1f16cc46eac2c10b9d6658986ab
Riajul Islam <riajulpislam@gmail.com>
2022-03-03 15:28:33 +0600

    arry

--- Array/arry.js
+++ Array/arry.js
@@ -1,4 +1,9 @@
-var name =['pavel','riajul','fahim','ashraf','rasel']
-name['rasel'] = 6
-console.log(name[3])
-console.log(name.length)
+var age = [1,2,3,4,5]
+age[5] = 10
+age[8] =11
+age[2] = 100
+console.log(age)
+console.log(age[7])
+console.log(age[0])
+console.log(age.length)
+
