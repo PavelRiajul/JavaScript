5aa864f13eb040419bb3560afaf03843f0ea7c7e
Riajul Islam <riajulpislam@gmail.com>
2022-03-13 23:45:35 +0600

    array

--- Array/arry.js
+++ Array/arry.js
@@ -1,14 +1,8 @@
-/*var name = ['pavel', 'Riajul', 'fahim', 'yamoli']
-
-for(var a = 0; a< name.length; a++){
-    console.log('Nmae:' + name[a])
-}*/
-var names = ['pavel', 'Riajul', 'fahim', 'yamoli']
-
-
-name.push('riajul islam')
-name.shift('pavel')
-
-console.log(names)
-
-
+var name = [1,2,3,4,5]
+name[5] = 10
+name[2] = 30
+console.log(name)
+console.log(0)
+console.log(name[5])
+console.log(name[7])
+console.log(name['length'])
