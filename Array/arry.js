3436cc0e7d00bc88e9d75e003c20be8b7c147923
Riajul Islam <riajulpislam@gmail.com>
2022-03-14 21:49:42 +0600

    var

--- Array/arry.js
+++ Array/arry.js
@@ -1,8 +1,12 @@
-var name = [1,2,3,4,5]
+/*var name = [1,2,3,4,5]
 name[5] = 10
 name[2] = 30
 console.log(name)
 console.log(0)
 console.log(name[5])
 console.log(name[7])
-console.log(name['length'])
+console.log(name['length'])*/
+var name = ['Riajul','islam','pavel','fahim']
+for(var i = 0; i < name.length; i++){
+    console.log('Name:' + name[i])
+}
--- Variable/var.js
+++ Variable/var.js
@@ -1,14 +1,1 @@
-/*var age = 20
-var _myName = 'Riaju islam pavel'
-var $collage = 'tamirul millat kamill madrasa'
-
-console.log(_myName +'i am 20 years old')
-console.log('My name is'+ _myName)
-console.log('My collage name is' + $collage)*/
-var name_age = 20 + 30 + 'riajul islam' + 40;
-console.log(name_age)
-console.log("Hello")
-var $age = 29
-var b = 30
-console.log($age)
-console.log(b)
+console.log(5)
