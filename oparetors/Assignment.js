94623edb03b4dc61dee729bff6a1fa7029e70fbf
Riajul Islam <riajulpislam@gmail.com>
2022-03-16 19:02:23 +0600

    oparetors-Assignment

--- oparetors/Assignment.js
+++ oparetors/Assignment.js
@@ -9,7 +9,15 @@ console.log(a)
 a %= b
 console.log(a)*/
 
-var a = 20
-var b = 10
-a /= b
-console.log(a)
+var x = 11
+var y = 10
+x -= y
+console.log(x)
+x += y
+console.log(x)
+x *= y
+console.log(x)
+x /= y
+console.log(x)
+x %= y
+console.log(x)
