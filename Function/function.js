66daa461e12eb756523c270703ff5c850e74f2b3
Riajul Islam <riajulpislam@gmail.com>
2022-03-18 15:40:38 +0600

    function

---
+++ Function/function.js
@@ -0,0 +1,14 @@
+function add() {
+    var a = 20
+    var b = 10
+    console.log(a + b)
+}
+
+function sub() {
+    var a = 30
+    var b = 20
+    console.log(a - b)
+}
+add()
+sub()
+sub()
--- object/basic-object.js
+++ object/basic-object.js
@@ -1,4 +1,4 @@
-var point = {
+/*var point = {
     x:20,
     y:100
 }
@@ -8,3 +8,11 @@ console.log(point)
 var prop = 'z'
 point[prop] = 500
 console.log(point)
+delete point.x
+console.log(point)*/
+var obj = {
+    normal:'Riajul islam'
+    name:['rahim','Karim','Rofiq']
+    age:[12,23,34,556]
+}
+console.log(obj)
