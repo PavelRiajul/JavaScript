d9a2694ae9538e693ff8bd03b4080b289503c7ac
Riajul Islam <riajulpislam@gmail.com>
2022-03-20 02:11:51 +0600

    function-basic

--- Function/function.js
+++ Function/function.js
@@ -1,14 +1,16 @@
-var arr1 = [1,2,3]
-var arr2 = [5,7,9]
-var arr3 = [9,6,4]
-function sumArray(arr) {
-    var sum = 0
-    for (var i= 0; i <arr.length; i++){
+/*var arr1 = [1,3,6]
+var arr2 = [4 ,7,9]
+var arr3 = [5,1,6]
+function  sum (arr){
+var sum = 0
+    for (var i = 0; i < arr.length; i++){
         sum += arr[i]
     }
     console.log(sum)
 }
-
-sumArray(arr1)
-sumArray(arr2)
-sumArray(arr3)
+sum(arr1)
+sum(arr2)*/
+function firstNmae (name, age) {
+    console.log('My Nmae is' + name + 'and I am' + age +'years old')
+}
+firstNmae('Riajul islam pavel',20)
