4515088158db19904a15735f34dc12acf966ef15
Riajul Islam <riajulpislam@gmail.com>
2022-05-20 20:17:42 +0600

    oparetor

--- data-type/null-undefined.js
+++ data-type/null-undefined.js
@@ -8,3 +8,6 @@ console.log(name)
 console.log(age)
 var complex = 200 + 200 + 10 + 'Riajul isla pavel' + 10 + 30
 console.log(complex)
+var name = 'Riajul islam'
+var age = name
+console.log(age)
--- oparetors/Arithmetic.js
+++ oparetors/Arithmetic.js
@@ -18,11 +18,23 @@ console.log(x - y)
 console.log(x * y)
 console.log(x / y)
 console.log(x % y)*/
-var a = 10
-var b = 6
+var a = parseInt('200')
+var b = 100
 console.log(a + b)
-console.log(a - b)
-console.log(a * b)
-console.log(a / b)
-console.log(a % b)
+var s = 100
+var z = 40
+console.log(s - z)
+var x = 200
+var k = 20
+console.log(x * k)
+var l = 1000
+ var o = 10
+console.log(l / o)
+var g = 199
+var p = 10
+console.log(g % p)
+console.log(g++)
+console.log(g)
+console.log(++p)
+
 
