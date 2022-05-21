6656aa55143beedb7457e397d54d3f9411f22510
Riajul Islam <riajulpislam@gmail.com>
2022-05-21 19:17:43 +0600

    operator

--- oparetors/Arithmetic.js
+++ oparetors/Arithmetic.js
@@ -18,23 +18,14 @@ console.log(x - y)
 console.log(x * y)
 console.log(x / y)
 console.log(x % y)*/
-var a = parseInt('200')
-var b = 100
+var a = 100
+b = 200
 console.log(a + b)
-var s = 100
-var z = 40
-console.log(s - z)
-var x = 200
-var k = 20
-console.log(x * k)
-var l = 1000
- var o = 10
-console.log(l / o)
-var g = 199
-var p = 10
-console.log(g % p)
-console.log(g++)
-console.log(g)
-console.log(++p)
-
-
+console.log(a -b)
+console.log(a * b)
+console.log(a / b)
+console.log(b % a)
+var x = 52
+z = 10
+console.log(x / z)
+console.log(x % z)
